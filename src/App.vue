5cae3a35317c0cc7b32c7431a256921bdd88ede2
Dashsoap <360062589@qq.com>
2019-05-09 12:46:40 +0800

    全局头

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
   <div id="app">
+    <the-toolbar v-if="isVisible"></the-toolbar>
     <router-view/>
     <the-footer></the-footer>
   </div>
 </template>
+<script>
+export default {
+  computed: {
+    isVisible() {
+      const nameList = ["home"];
+      return !nameList.includes(this.$route.name);
+    }
+  }
+};
+</script>
 
 <style lang="scss">
 </style>
