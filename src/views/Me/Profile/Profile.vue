9791acab2c4589b2bae014ec6893f5db768a744c
Dashsoap <360062589@qq.com>
2019-05-11 17:13:13 +0800

    user完成

--- src/views/Me/Profile/Profile.vue
+++ src/views/Me/Profile/Profile.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
+    1{{me}}1
     <el-form :model="me" label-width="24%" label-position="left" size="large">
       <el-form-item label="用户名">
         <el-input v-model="me.username" autocomplete="off" disabled></el-input>
       </el-form-item>
       <el-form-item label="昵称">
-        <el-input v-model="me.nikename" autocomplete="off"></el-input>
+        <el-input v-model="me.nickname" autocomplete="off"></el-input>
       </el-form-item>
       <el-form-item label="手机号">
         <el-input v-model="me.phoneNumber" autocomplete="off"></el-input>
       </el-form-item>
     </el-form>
+    <el-button type="primiry" @click="onSubmit">提交</el-button>
   </div>
 </template>
 
 <script>
+import { AxiosDefault } from "../../../plugins/axios.js";
+
 export default {
   data() {
     return {
@@ -24,6 +28,25 @@ export default {
   created() {
     this.me = this.$store.state.me;
     console.log(this.me);
+  },
+  methods: {
+    async onSubmit() {
+      const id = this.me.data;
+      const data = {
+        nickname: this.me.nickname,
+        phoneNumber: this.me.phoneNumber
+      };
+      const body = {
+        id: id,
+        data: data
+      };
+      const res = await AxiosDefault.put("api/user", data);
+      if (!res.data.body.code) {
+        this.$message.success(`成功!`);
+      } else {
+        this.$message.warning(`失败!`);
+      }
+    }
   }
 };
 </script>
